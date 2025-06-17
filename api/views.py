d40b05c221a3378e662dd24b20315c2314b6cde3
MIS App Developer <mis-app-developer@MISs-Mac-mini.local>
2025-06-17 15:24:35 +0600

    Fixed Date

--- api/views.py
+++ api/views.py
@@ -1,8 +1,15 @@
-from django.http import JsonResponse
-from django.shortcuts import render
-from students.models import Student
+from rest_framework.response import status
+
+@api_view(['GET', 'POST'])
 def studentView(request):
-    students=Student.objects.all()
-    print(students)
-    students_list=list(students.values())
-    return JsonResponse(students_list,safe=False)
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+        
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
