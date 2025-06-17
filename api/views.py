aa587e93a8aeb13910f8318ed694587d60402f5a
MIS App Developer <mis-app-developer@MISs-Mac-mini.local>
2025-06-17 16:55:46 +0600

    Fixed Date

--- api/views.py
+++ api/views.py
@@ -5,9 +5,17 @@ from rest_framework.decorators import api_view
 from students.models import Student
 from .serializers import StudentSerializer
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def studentView(request):
     if request.method == 'GET':
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method=='POST':
+        serializer=StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            
